7b1ad9531facfb44a25a366468b5c7e34e8d9fc0
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2023-07-26 12:23:30 +0530

    added the proxy to the flight -- now i can avail the flight sevices from the api gateway directly

--- index.js
+++ index.js
@@ -12,7 +12,24 @@ const limiter = rateLimit({
 
 app.use(morgan('combined'));
 const { createProxyMiddleware } = require('http-proxy-middleware');
+//below will restrict the number of times a user with a ip can make request in certain time window
 app.use(limiter);
+
+/*
+accoding to our scope of the project we can have these type of request from our website
+1 -> either a user want to search for a flight so he can do this without any authentication or authorization
+so for searching the flight we have to redirect him to the flight and search service
+
+2 -> or a user want to book a ticket so he must be authorized and authenticated as a cutomer
+meaning at the time of booking he must be logged in
+
+3 -> or a user want to see his already booked ticket althought at the time of booking we will send
+the ticket to the registered email but he anyways want to see his ticket then we have to redirect him to the 
+remainder service
+*/
+
+//1
+
 
 
 app.use('/booking' , async (req,res,next) => {
@@ -26,6 +43,7 @@ app.use('/booking' , async (req,res,next) => {
         console.log(response.data.sucess);
         if(response)
             next();
+            
     } catch (error) {
         // console.log(error.response.data.message)
         // if(error.response.data.message == 'User is not authenticated to perform activity'){
@@ -36,7 +54,7 @@ app.use('/booking' , async (req,res,next) => {
         // }
 
         if(error.response.data.message == 'User is not authenticated to perform activity'){
-            return res.json({
+            return res.status(401).json({
                 error : error.response.data
             })
         }
@@ -48,6 +66,8 @@ app.use('/booking' , async (req,res,next) => {
 })
 
 app.use('/booking', createProxyMiddleware({ target: 'http://localhost:5600', changeOrigin: true }));
+
+app.use('/flight', createProxyMiddleware({ target: 'http://localhost:4003', changeOrigin: true }));
 
 const startServer = async () => {
 
